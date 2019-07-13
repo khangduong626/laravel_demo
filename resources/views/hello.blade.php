3136053505f33633f532aaa846179760d29e5243
Khang Duong <khangdxph06592@fpt.edu.vn>
2019-07-13 20:24:42 +0700

    lab2

---
+++ resources/views/hello.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+</head>
+<body>
+	<table class="table table-bordered">
+		<tr>
+			<td>Tên: </td>
+			<td>{{ $ten }}</td>
+		</tr>
+		<tr>
+			<td>Tuổi</td>
+			<td>{{ $tuoi}}</td>
+		</tr>
+		<tr>
+			<td>Địa chỉ</td>
+			<td>{{ $diachi}}</td>
+		</tr>
+		<tr>
+			<td>Lớp</td>
+			<td>{{ $lop}}</td>
+		</tr>
+	</table>
+
+</body>
+</html>
